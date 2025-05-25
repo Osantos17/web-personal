6045c895460030262cc6647f660cc32c0825ee44
Orlando Santos <Santos.Orlando17@gmail.com>
2025-05-25 01:04:51 -0700

    add responsive fix to wood images and reduced size

--- src/components/Woodworking.tsx
+++ src/components/Woodworking.tsx
@@ -11,36 +11,27 @@ function WoodWorking() {
         Woodworking for me is about creating pieces that are both beautiful and functional. Every detail matters, once the first cut is made, there's no turning back. Crafting my son’s crib from a walnut slab was my proudest and most challenging project. While some builds focus more on function than design, the same level of planning and precision goes into every piece.
       </div>
 
-      <div className="w-1/2 ml-auto mt-1 lg:w-2/5">
+      <div className="woodImages w-1/2 ml-auto mt-1 lg:w-2/5">
         <Swiper
-          scrollbar={{
-            hide: false,
-
-          }}
-          grabCursor={true}     
-          resistance={true}     
-          resistanceRatio={0.5} 
-          threshold={1}         
-          speed={500}           
+          scrollbar={{ hide: false }}
+          grabCursor={true}
           modules={[Scrollbar]}
           className="mySwiper"
         >
-
-          <SwiperSlide className="md:mb-6 lg:mb-16">
+          <SwiperSlide>
             <img
               src="/images/CribCoop.png"
               alt="CribCoop"
-              className="max-h-55 max-w-full mb-4 md:max-w-58 md:max-h-100 lg:max-w-100 lg:max-h-175"
+              className="woodworking-image"
             />
           </SwiperSlide>
-          <SwiperSlide className="lg:mb-16">
+          <SwiperSlide>
             <img
               src="/images/FireToddler.png"
               alt="FireToddler"
-              className="max-h-55 max-w-full mb-4 md:max-w-58 md:max-h-100 lg:max-w-100 lg:max-h-175"
+              className="woodworking-image"
             />
           </SwiperSlide>
-
         </Swiper>
       </div>
     </div>
